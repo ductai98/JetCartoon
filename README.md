e352e59f7b4fd1dc65c4eb95b02ed3ef7d909ab7
ductai <lductai98@gmail.com>
2024-10-29 16:28:46 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# JetCartoon
+
+Use rick and morty api into my android project
+
+- Jetpack Compose
+- Ktor
+- MVVM
+- Clean Architect
---
+++ app/src/main/java/com/taild/jetcartoon/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.taild.jetcartoon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.taild.jetcartoon.ui.theme.JetCartoonTheme
+import com.taild.network.Character
+import com.taild.network.KtorClient
+
+class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            var character by remember {
+                mutableStateOf<Character?>(null)
+            }
+
+            LaunchedEffect(Unit) {
+                character = ktorClient.getCharacters(1)
+            }
+
+            JetCartoonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+
+                    Text(text = character?.name ?: "No character found")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetCartoonTheme {
+        Greeting("Android")
+    }
+}
