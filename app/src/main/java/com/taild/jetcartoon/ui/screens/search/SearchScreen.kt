bd974179a914c159d7d6bf18a71776124b0cdbae
ductai <lductai98@gmail.com>
2024-11-15 16:02:31 +0700

    Search screen

--- app/src/main/java/com/taild/jetcartoon/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/taild/jetcartoon/ui/screens/search/SearchScreen.kt
@@ -1,6 +1,8 @@
 package com.taild.jetcartoon.ui.screens.search
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,19 +11,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.input.delete
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.taild.jetcartoon.components.SimpleToolbar
+import com.taild.jetcartoon.ui.theme.RickAction
 import com.taild.jetcartoon.ui.theme.RickPrimary
 
 @Composable
@@ -60,9 +70,36 @@ fun SearchScreen(
                         tint = RickPrimary
                     )
                     BasicTextField(
-                        state = viewModel.searchTextFieldState
+                        state = viewModel.searchTextFieldState,
+                        modifier = Modifier.weight(1f),
                     )
+
+                    AnimatedVisibility(
+                        visible = viewModel.searchTextFieldState.text.isNotBlank()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Delete,
+                            contentDescription = "Delete",
+                            tint = Color.Gray,
+                            modifier = Modifier.clickable {
+                                viewModel.searchTextFieldState.edit {
+                                    delete(0, length)
+                                }
+                            }
+                        )
+                    }
                 }
+
+                val searchText by viewModel.searchTextState.collectAsStateWithLifecycle()
+                Text(
+                    text = searchText,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 26.sp
+                )
             }
         }
     }
